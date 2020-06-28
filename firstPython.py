9e79fdd8066e24fa4bbab792f66c6b692d20857b
Jake Krayger <jkrayg@protonmail.com>
2020-06-28 16:16:44 -0400

    adding files

--- firstPython.py
+++
@@ -1,6 +0,0 @@
-a = ["a", "b", "c", "d"]
-b = True
-c = 4
-
-if b :
-    print(a[3])
---
+++ python_sandbox_starter/variables.py
@@ -0,0 +1,43 @@
+# A variable is a container for a value, which can be of various types
+"""
+VARIABLE RULES:
+  - Variable names are case sensitive (name and NAME are different variables)
+  - Must start with a letter or an underscore
+  - Can have numbers but can not start with one
+"""
+
+#------------------------------------------------------------------------------------
+#====================================================================================
+#------------------------------------------------------------------------------------
+
+'''
+This is a 
+multiline comment
+or docstring (used to define a functions purpose)
+can be single or double quotes
+'''
+
+# a hashtag is how you comment
+#------------------------------
+
+x = 1           #int
+y = 2.5         #float    #---------------------------------
+name = "jake"   #string                                    |
+is_cool = True  #boolean                                   |
+                                                          #|---- Affectively the same thing
+                                                          #|
+x, y, name, is_cool = (1, 2.5, "jake", True)  #-------------
+
+#Basic math
+a = x * y
+
+#Casting
+x = str(x)
+
+
+
+
+
+
+
+print(x)
